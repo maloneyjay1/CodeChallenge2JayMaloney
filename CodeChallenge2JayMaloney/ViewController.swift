77080ef94905a7214b06a087c3ccc2d29cfac39a
Jay Maloney <maloneyjay1@gmail.com>
2016-04-14 08:45:34 -0400

    Initial Commit

---
+++ CodeChallenge2JayMaloney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeChallenge2JayMaloney
+//
+//  Created by Jay Maloney on 4/14/16.
+//  Copyright © 2016 Jay Maloney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
